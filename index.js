d8c27609fc6d0e61fa9b442e7fafd274c173815e
Adaevs <51508583+Adaevs@users.noreply.github.com>
2019-06-08 18:03:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const http = require('http'); // переменная или константа и модуль  
+const http = require('http'); 
 
-const server = http.createServer(function(req, res) {  // Создание сервера так же запрос и выдача
-    res.writeHead(200, {'content-type': 'text/plain; charset=utf-8'}); //1. запрос от сервера, 2. что будем выводить, 3. кодировка для текста. Заместо plain можно вставить html или любой другой файл. 
-    res.end('Привет мир!'); // выводим информацию
+const server = http.createServer((req, res) => { 
+    res.end('Привет мир!'); 
 }); 
 
-server.listen(3000, '127.0.0.1'); // port
-    console.log("Мы отслеживаем порт 3000"); //вывод в терминал.
+server.listen(3000); 
+    console.log("Мы отслеживаем порт 3000");
