6ac3ccfe378c2affd9e9975ade8cea6252089c3b
Adaevs <51508583+Adaevs@users.noreply.github.com>
2019-06-09 17:30:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => { 
-    switch (req.url) {
-        case '/statistic':
-    res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
-    res.end('по запросу '  + req.url +  ' не найдена статистика');
-    
-    case '/world': 
-    res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
-    res.end("world");
+res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
 
-    default:
-    res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
-    res.end('Не найден обработчик по вашему запросу');
+switch (req.url) {
+case '/statistic':
+res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
+res.end('по запросу '  + req.url +  ' не найдена статистика');
+console.log("по запросу '  + req.url +  ' не найдена статистика");
+    
+case '/world': 
+res.end("world");
+console.log('world');
+
+default:
+res.end('Не найден обработчик по вашему запросу');
+console.log("Не найден обработчик по вашему запросу");
 }
-}); 
+
+});
 server.listen(3000);
 console.log("Мы отслеживаем порт 3000");
-
