5ea265348d6fe6f7264d2b01cb42c473f39f7eec
Adaevs <51508583+Adaevs@users.noreply.github.com>
2019-06-09 15:44:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => { 
-    console.log("URL страницы: " + req.url);
-    res.end('Я надеюсь ты имел ввиду в терминале'); 
+    switch (req.url) {
+        case '/statistic':
+    res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
+    res.end('по запросу '  + req.url +  ' не найдена статистика');
+    
+    case '/world': 
+    res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
+    res.end("world");
+
+    default:
+    res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
+    res.end('Не найден обработчик по вашему запросу');
+}
 }); 
-server.listen(3000, ); 
+server.listen(3000);
 console.log("Мы отслеживаем порт 3000");
+
