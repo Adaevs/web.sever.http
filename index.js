8ae17121d9471ef752564df7e5f8ec4430591727
Adaevs <51508583+Adaevs@users.noreply.github.com>
2019-06-10 14:03:25 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const http = require('http'); 
-const url = require('url');
+const http = require('http');
 
-const server = http.createServer((req, res) => { 
-  res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
-
+const server = http.createServer((req, res) => {
   switch (req.url) {
-    case '/statistic':
-      res.writeHead(200, {'content-type': 'text/plane ; charset=utf-8'});
-      res.end('по запросу '  + req.url +  ' не найдена статистика');
-        console.log("по запросу "  + req.url +  " не найдена статистика");
-
-    case '/world':
-      res.end("world");
-        console.log('world');
-
-    default:
-      res.end('Не найден обработчик по вашему запросу');
-        console.log("Не найден обработчик по вашему запросу");
-}
-
+  case '/statistic': {
+    res.writeHead(200, { 'content-type': 'text/plane; charset=utf-8 ' });
+    res.end('По запросу /statistic не найдена статистика');
+    console.info('По запросу /statistic Не найдена статистика'); break; }
+  case '/world': {
+    res.end('world');
+    console.info('world');
+    break; }
+  default: {
+    res.end('Не найден обрабочик ппо вашему запросу');
+    console.info('Не найден обработчик по вашему запросу');
+    break;
+  }
+  }
 });
 server.listen(3000);
-        console.log("Мы отслеживаем порт 3000");
+console.info('Мы отслеживаем порт 3000');
