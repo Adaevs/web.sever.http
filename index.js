a2a53a4929be2725b6ce13719a67634331bebbc3
Adaevs <51508583+Adaevs@users.noreply.github.com>
2019-06-16 16:33:31 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-  case '/statistic': {
-    res.writeHead(200, { 'content-type': 'text/plane; charset=utf-8 ' });
-    res.end('По запросу /statistic не найдена статистика');
-    console.info('По запросу /statistic Не найдена статистика'); break; }
-  case '/world': {
-    res.end('world');
-    console.info('world');
-    break; }
-  default: {
-    res.end('Не найден обрабочик ппо вашему запросу');
-    console.info('Не найден обработчик по вашему запросу');
+  res.writeHead(200, { 'content-type': 'text/plane ; charset=utf-8' });
+
+  switch (req.method) {
+  case 'GET': {
+    res.end(req.method);
+    console.info(req.method);
     break;
   }
+  case 'POST': {
+    res.end('POST');
+    console.info(req.method);
+    break; }
+  case 'PUT': {
+    res.end('PUT');
+    console.info('PUT');
+    break;
   }
-});
-server.listen(3000);
-console.info('Мы отслеживаем порт 3000');
+  case 'SELECT': {
+    res.end('SELECT');
+    console.info('SELECT'); break; }
+  case 'DELETE': {
+    res.end('DELETE');
+    console.info('DELETE');
+    break;
+  }
+  default: {
+    res.end('Не найден обработчик по вашему запросу');
+    console.info(req.method); break; }
+  }
+}); server.listen(3000); console.info('Мы отслеживаем порт 3000');
